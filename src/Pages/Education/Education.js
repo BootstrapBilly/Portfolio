1eb724e9e65fb29e6f39f4c1051b7bfab3d0d401
Billy <billyjcatchpole@gmail.com>
2020-03-26 13:35:34 +0000

    Education UI Ui for all devices now complete Added in a footer vector to fill some whitespace

--- src/Pages/Education/Education.js
+++ src/Pages/Education/Education.js
@@ -7,6 +7,9 @@ import classes from "./Education.module.css"
 //components
 import Nav from "../../Containers/Nav/Nav"
 
+//assets
+import photo from "../../Assets/Img/education.svg"
+
 const Home = props => {
 
     return (
@@ -15,14 +18,12 @@ const Home = props => {
 
             <Nav path={props.location.pathname} />
 
-            <h1 className={classes.title}>Education</h1>
-
             <div className={classes.container}>
 
                 <div className={classes.university}>
 
                     <h3 className={classes.subject}>Computer Science - 2015-2021</h3>
-    
+
                     <h2 className={classes.school_name}>University of Hertfordshire </h2>
 
                     <p className={classes.degree_description}>This flexible, online bachelor's degree has provided a strong foundation of problem solving and software engineering. </p>
@@ -41,14 +42,22 @@ const Home = props => {
 
                 <div className={classes.college}>
 
-                    <h3 className={classes.subject}>Business Studies - A*/A*/A</h3>
+                    <h3 className={[classes.subject, classes.subject_college].join(" ")}>Business Studies - A*/A*/A</h3>
                     <h2 className={classes.school_name}>Chelmsford College </h2>
+                    
+                    <div className={classes.school}>
 
+                        <h3 className={[classes.subject, classes.subject_school].join(" ")}>10 A*-C GCSE's</h3>
+                        <h2 className={classes.school_name}>Jo Richardson community school</h2>
+
+                    </div>
 
                 </div>
 
-            </div>
 
+
+            </div>
+            <div className={classes.footer_image_wrapper}><img className={classes.footer_image} src={photo} alt="An education graphic" /></div>
         </React.Fragment>
 
     )
