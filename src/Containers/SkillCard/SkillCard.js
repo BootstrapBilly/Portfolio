8564ed144e25c562df3044c36f38aad2f1412ceb
Billy <billyjcatchpole@gmail.com>
2020-03-26 16:51:19 +0000

    Skills UI Design Finished the intial draft for the skills tab Added new component - Skillcard : holds a particular skill

---
+++ src/Containers/SkillCard/SkillCard.js
@@ -0,0 +1,39 @@
+//core react
+import React from "react"
+
+//css
+import classes from "./SkillCard.module.css"
+
+
+
+const SkillCard = props => {
+
+    return (
+
+        <div className={classes.container}>
+
+            <div className={classes.title_container}>
+
+                <span className={classes.title} style={{color:props.color}}>{props.title}</span>
+
+            </div>
+
+            <div className={classes.icon_container}>
+
+                <img className={classes.icon} src={props.icon_name} alt="A Navigation icon" />
+
+            </div>
+
+            <div className={classes.button_container}>
+
+                <div className={classes.button} style={{backgroundColor:props.color}}>More info</div>
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export default SkillCard
